eeb243632bc155ef5f82356b9c83aa29c2b1a4d5
wh0-1-am <o.t5@hotmail.com>
2018-11-11 19:05:49 +0300

    ee

--- config/routes.rb
+++ config/routes.rb
@@ -17,10 +17,10 @@ Rails.application.routes.draw do
   # get "departments/edit"
 
   resources :departments
+  root "departments#index"
   resources :courses
   resources :semesters
   resources :contents
-  root "departments#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
