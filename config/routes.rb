d3a8d217777c545024f729ec3bc928af5909ae85
wh0-1-am <o.t5@hotmail.com>
2018-11-11 15:05:00 +0300

    add root

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,6 @@ Rails.application.routes.draw do
   resources :courses
   resources :semesters
   resources :contents
+  root "departments/index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
